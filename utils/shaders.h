3e39702e3c1bdc3ee01de9d33f8e835ae62ce4aa
VIMONT ludovic <ludovic.vimont@gmail.com>
2015-03-26 13:18:46 +0100

    Delete old files

--- utils/shaders.h
+++
@@ -1,6 +0,0 @@
-#ifndef __SHADERS_H__
-#define __SHADERS_H__ 
-#include <string>
-GLuint make_program(const std::string& vertexname, const std::string& fragmentname);
-void link_program(GLuint program);
-#endif // __SHADERS_H__
--- utils/utils.h
+++
@@ -1,6 +0,0 @@
-#ifndef __UTIL_H__
-#define __UTIL_H__ 
-void checkGLerror();
-#endif // __UTIL_H__
-
-
