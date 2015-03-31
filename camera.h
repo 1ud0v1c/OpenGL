899d40b69b2282ca75f08edb933dcfa53c47b1d3
f4r3n <guillaume_k@hotmail.fr>
2015-03-31 10:47:31 +0200

    camera

--- camera.h
+++ camera.h
@@ -5,4 +5,32 @@
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp> 
 #include <glm/gtc/matrix_transform.hpp> 
-#
+#include "glfwMinimal.h"
+
+#include <iostream>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtc/constants.hpp>
+#include <GL/glew.h> // must be included before gl.h
+#include <GLFW/glfw3.h>
+#include <fstream>
+#include <vector>
+class Camera {
+	public:
+		Camera();
+		void update(float deltaTime, GLFWwindow *window);
+		glm::mat4 getProjection();
+		glm::mat4 getView();
+	private:
+		glm::vec3 position = glm::vec3( 0, 0, 5 );
+		float horizontalAngle = 3.14f;
+		float verticalAngle = 0.0f;
+		float initialFoV = 45.0f;
+		double xpos, ypos;
+		float speed = 3.0f; // 3 units / second
+		float mouseSpeed = 0.009f;
+		glm::mat4 projection;
+		glm::mat4 view;
+};
+#endif
